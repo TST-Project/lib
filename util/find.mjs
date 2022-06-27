8ece0cb059cc4a93fa7c4450dcb6dc0055d24682
chchch <cchl2@cam.ac.uk>
2022-06-27 12:28:01 +0200

    debug

--- util/find.mjs
+++ util/find.mjs
@@ -119,7 +119,7 @@ const find = {
     tbcs: (xmlDoc) => xmlDoc.querySelectorAll('seg[function="TBC"]'),
 
     title: (xmlDoc) => {
-        if(!xmlDoc.querySelector('titleStmt > title')) console.log(xmlDoc.querySelector('titleStmt'));
+        if(!xmlDoc.querySelector('titleStmt > title')) console.log(xmlDoc.querySelector('idno').innerHTML);
         return xmlDoc.querySelector('titleStmt > title').textContent.replace(/&/g,'&#38;').trim()
     },
 
